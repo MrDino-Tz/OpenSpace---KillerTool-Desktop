e15087248b7fe9506bf6924a9805086719416ec3
MrDino-Tz <mrdinotz@gmail.com>
2026-06-20 13:09:29 +0300

    fixes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,9 +7,9 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(debug_assertions)]
-            app.get_webview_window("main").unwrap().open_devtools();
+            _app.get_webview_window("main").unwrap().open_devtools();
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![close_app])
